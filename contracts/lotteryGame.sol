b381cf66ce98122d6882c9479cba633c87c78e1c
Hope Mabuza <hopemabuza@Hopes-MacBook-Pro.local>
2026-02-10 22:02:08 +0200

    added state variables for contract

--- contracts/lotteryGame.sol
+++ contracts/lotteryGame.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.20;
 
 contract lotteryGame {
-    //variables
-    address public owner;
-    uint256 public  ticketPrice;
-    uint256 public cuurentRound;
+    //state variables
+    address public owner;//contract must know who perfoms administrative actions, e.g trigerring the ChainLink
+    uint256 public  ticketPrice = 0.00002 ether;//constant ticket price, will be used to calculations for reward distribution
+    uint256 public cuurentRound;//keeps track of data of each round
+    bool public roundOpen;//restricts the time for buying tickets for each round
+    uint256 public totalPoolAMount;//total of the current pool
+
+
 
     
 
