8e8e8378b39929a7e1f981a55f6c8c3f3d03c1f7
Hope Mabuza <hopemabuza@Hopes-MBP.itried.local>
2026-02-10 15:39:55 +0200

    added state variables

---
+++ contracts/lotteryGame.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract lotteryGame {
+    //variables
+    address public owner;
+    uint256 public  ticketPrice;
+    uint256 public cuurentRound;
+
+    
+
+}
